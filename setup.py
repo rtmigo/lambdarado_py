7ed38677ee2d48bba2e48dee5c48e42c1d3bb55d
rtmigo <art.galkin@gmail.com>
2021-05-11 05:44:22 +0300

    docs

--- setup.py
+++ setup.py
@@ -15,7 +15,11 @@ setup(
     install_requires=['apig_wsgi', 'awslambdaric'],
     packages=['lambdarado'],
 
-    description="Universal entrypoint for containerized AWS Lambda apps",
+    description="Universal entry point for Docker images containing "
+                "a WSGI app for the AWS Lambda.",
+
+    keywords="amazon aws lambda function entrypoint docker image container "
+             "wsgi flask http api gateway".split(),
 
     long_description=(Path(__file__).parent / 'README.md').read_text(),
     long_description_content_type='text/markdown',
@@ -23,6 +27,7 @@ setup(
     license='MIT',
 
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
