40669fbb22ab017003435736cf6350478118504e
rtmigo <art.galkin@gmail.com>
2021-05-11 04:21:21 +0300

    ci

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup
 setup(
     name="lambdarado",
     version="0.0.0",
-    
     author="Artёm IG",
     author_email="ortemeo@gmail.com",
     url='https://github.com/rtmigo/lambdarado_py',
